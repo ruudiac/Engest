f2e8d18be9a663cc2e2feca871dae5539788aeff
ruudiac <gertrudroos7@gmail.com>
2025-04-14 15:03:47 +0300

    Test

--- Engest/ContentView.swift
+++ Engest/ContentView.swift
@@ -9,16 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("Engest.")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                Text("Just learn.")
+                    .foregroundColor(.white)
+                    .padding(.bottom, 20)
+                
+                NavigationLink(destination: FirstView()) {
+                    Text("Sign up")
+                        .padding()
+                        .frame(width:150, height: 50)
+                        .background(Color(red: 161/255, green: 85/255, blue: 203/255))
+                        .foregroundColor(.white)
+                        .cornerRadius(40)
+                }
+                
+                NavigationLink(destination: SecondView()) {
+                    Text("Log in")
+                        .padding()
+                        .frame(width:150, height: 50)
+                        .background(Color(red: 161/255, green: 85/255, blue: 203/255))
+                        .foregroundColor(.white)
+                        .cornerRadius(40)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(red: 0.796, green: 0.667, blue: 0.871))
+            .ignoresSafeArea()
         }
-        .padding()
     }
 }
-
-#Preview {
-    ContentView()
-}
+        
+        #Preview {
+            ContentView()
+        }
