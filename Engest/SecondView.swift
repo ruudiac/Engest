baaf4183f892f57992a46e053ac86d54acb026e1
ruudiac <gertrudroos7@gmail.com>
2025-05-08 01:53:24 +0300

    creating an authentication

--- Engest/SecondView.swift
+++ Engest/SecondView.swift
@@ -7,11 +7,28 @@
 
 import SwiftUI
 
+@MainActor
 final class SignInEmailViewModel: ObservableObject {
     
     @Published var email = ""
     @Published var password = ""
     
+    func signIn(){
+        guard !email.isEmpty, !password.isEmpty else {
+            print("No email or password found!")
+            return
+        }
+        
+        Task {
+            do {
+                let returnedUserData = try await AuthenticationManager.shared.createUser(email: email, password: password)
+                print("Success")
+                print(returnedUserData)
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+    }
 }
 
 struct SecondView: View {
@@ -26,19 +43,17 @@ struct SecondView: View {
                 
                 
                 TextField("Email:", text: $viewModel.email)
-                    .textFieldStyle(.roundedBorder)
                     .padding()
-                    .frame(width: 250)
+                    .background(Color(red: 161/255, green: 85/255, blue: 203/255).opacity(0.1))
                     .cornerRadius(40)
                 
                 SecureField("Password:", text: $viewModel.password)
-                    .textFieldStyle(.roundedBorder)
                     .padding()
-                    .frame(width: 250)
+                    .background(Color(red: 161/255, green: 85/255, blue: 203/255).opacity(0.1))
                     .cornerRadius(40)
                 
                 Button{
-                    
+                    viewModel.signIn()
                 } label: {
                     Text("Sign In")
                         .padding()
